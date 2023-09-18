ba8774a7d65123ca2f5d825cc2b184cd868b3817
James Guppy <j.guppy321@gmail.com>
2023-09-18 23:07:54 +0100

    Fixed proj card issue where the projects array index wouldn't always be correct when clicking through. This resulted in some strange behaviour where projects would be skipped and have a delay in loading data.

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,24 +4,21 @@ import { projects } from '../data/ProjectsData'
 function ProjectCard() {
   
   const [projectsIndex, setProjectsIndex] = React.useState(0)
-  const [selectedProject, setSelectedProject] = React.useState(projects[0])
 
   const handleNext = () => {
     if (projectsIndex >= (projects.length - 1)) {
       setProjectsIndex(0)
     } else if (projectsIndex <= (projects.length - 1)) {
-      setProjectsIndex(projectsIndex + 1)
+      setProjectsIndex(prev => prev + 1)
     }
-    setSelectedProject(projects[projectsIndex])
   }
 
   const handlePrevious = () => {
     if (projectsIndex <= 0) {
       setProjectsIndex(projects.length - 1)
     } else {
-      setProjectsIndex(projectsIndex - 1)
+      setProjectsIndex(prev => prev - 1)
     }
-    setSelectedProject(projects[projectsIndex])
   }
 
 
@@ -32,25 +29,25 @@ function ProjectCard() {
         <div className="column is-two-fifths">
           <h1 className="title"><span className="purple-text">P</span>rojects</h1>
           <figure className="image is-16by9">
-            <img src={selectedProject.image} alt={selectedProject.title}/>
+            <img src={projects[projectsIndex].image} alt={projects[projectsIndex].title}/>
           </figure>
         </div>
         <div className="column is-three-fifths">
           <div className="proj-text-container">
-            <h1 className="title">{selectedProject.title}</h1>
+            <h1 className="title">{projects[projectsIndex].title}</h1>
             <p className="text">
-              {selectedProject.info}
+              {projects[projectsIndex].info}
             </p>
             <div className="proj-techs">
-              {selectedProject.techIcons.map(icon =>(
+              {projects[projectsIndex].techIcons.map(icon =>(
                 <i key={icon} className={icon}></i>
               ))}
             </div>
             <div className="project-links">
-              <a href={selectedProject.deployedLink} className="button is-rounded proj-button">
+              <a href={projects[projectsIndex].deployedLink} className="button is-rounded proj-button">
                 <i className="devicon-firefox-plain small-icon"></i>
               </a>
-              <a href={selectedProject.github} className="button is-rounded proj-button">
+              <a href={projects[projectsIndex].github} className="button is-rounded proj-button">
                 <i className="devicon-github-original small-icon"></i>
               </a>
             </div>
