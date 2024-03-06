894cefa664d4879bf4f1b34762f7dfd03f0f2c68
James Guppy <j.guppy321@gmail.com>
2024-03-06 13:39:30 +0000

    Updated about me copy.

--- src/data/AboutMeBio.js
+++ src/data/AboutMeBio.js
@@ -4,15 +4,15 @@ import React from 'react'
 function AboutMe() {
   return (
     <p className="text">
-      My journey into coding started when I was 16 and studying for my A-Levels. I picked up some Visual Basic and Pascal during an IT class and immediately fell in love with building small applications and problem solving.<br/>
+      My journey into coding started when I was 16 and studying for my A-Levels. I picked up some Visual Basic and Pascal during an IT class and immediately fell in love with building simple applications and problem solving.<br/>
       <br/>
 
 
-      Whilst my working knowledge of those two languages has sadly been lost to time, my passion has not. After spending four years studying a degree in Japanese and Korean, and almost two years working in marketing, I felt the itch to flex my brain again by returning to coding.<br/>
+      After spending four years studying a degree in Japanese and Korean at SOAS, University of London, and two years working in a marketing role in the video game industry, I felt the itch to flex my brain again and return to coding.<br/>
       <br/>
 
 
-      Having finished a three month intensive Software Engineering Course with General Assembly, I was able to solidify my fullstack web development skills. During this time I built a range of projects using various frontend and backend technologies. I feel most at home on the MERN stack, but I’m also proficient at building REST and GraphQL APIs using Django and have some experience with Ruby.<br/>
+      Having over two years of full stack web development experience in the affiliate marketing industry, I was able to solidify a range of technical and personal skills. During this time I built a range of projects using various frontend and backend technologies. I feel most at home using MERN technologies and building RESTful and GraphQL APIs in Django. On top of this I also have some experience coding in Ruby which I’d love an opportunity to develop further.<br/>
       <br/>
 	    Check out my past and current projects below and feel free to get in touch!
     </p>
